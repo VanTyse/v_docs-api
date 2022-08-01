fcca4080c28c181a5b01e0c7cab01ec2b86faf37
Kingsley Okafor <83710137+VanTyse@users.noreply.github.com>
2022-08-01 15:58:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/v1/users/', userRoutes)
 app.use('/api/v1/documents/', documentRoutes)
 
 
-//function to find or create document
+//function to find or create new document in db
 const findOrCreateDocument = async (documentID, userID, documentName) => {
     if (documentID == null)return
     const document = await Document.findOne({documentID})
@@ -47,19 +47,16 @@ const findOrCreateDocument = async (documentID, userID, documentName) => {
 //function to check if user can edit document
 const canEdit = ({userID, creator, collaborators}) => {
     if (userID == null)return false
-    console.log(creator.toString(), userID)
-    console.log(collaborators.includes(userID), creator.equals(userID));
     return creator.equals(userID) || collaborators.includes(userID)
 }
 
 //initialize socket connection
 io.on('connection', socket => {
-    console.log('connected')
     socket.on('get-document', async (documentID, userID, documentName) => {
         const document = await findOrCreateDocument(documentID, userID, documentName)
         const {data, collaborators, creator, documentName : name} = document
         const canUserEdit = canEdit({userID, creator, collaborators})
-        console.log(canUserEdit);
+
         //In the future I might decide to add a condition to join so that unauthorized users can't
         // subscribe to changes in real time
         socket.join(documentID)
@@ -76,6 +73,7 @@ io.on('connection', socket => {
 
 
 //start server and define port to listen on
-server.listen(3001, () => {
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
     console.log('server connected to port 3001')
 })
